31a893c7486f91789941e095febe27ae9e988284
NhamMatOTho <Hoangtrongluoi@gmail.com>
2024-09-20 20:26:17 +0700

    Add pause menu

---
+++ Assets/Scrips/UI/UIManager.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : SingletonMonobehavior<UIManager>
+{
+    private bool _pauseMenuOn = false;
+    [SerializeField] private GameObject pauseMenu = null;
+    [SerializeField] private GameObject[] menuTabs = null;
+    [SerializeField] private Button[] menuButtons = null;
+
+    public bool PauseMenuOn { get => _pauseMenuOn; set => _pauseMenuOn = value; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        pauseMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        PauseMenu();
+    }
+
+    private void PauseMenu()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PauseMenuOn)
+            {
+                DisablePauseMenu();
+            }
+            else
+            {
+                EnablePauseMenu();
+            }
+        }
+    }
+
+    private void DisablePauseMenu()
+    {
+        PauseMenuOn = false;
+        Player.Instance.PlayerInputIsDisabled = false;
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    private void EnablePauseMenu()
+    {
+        PauseMenuOn = true;
+        Player.Instance.PlayerInputIsDisabled = true;
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+
+        //trigger Garbage collecter
+        System.GC.Collect();
+
+        HighlightButtonForSelectedTab();
+    }
+
+    private void HighlightButtonForSelectedTab()
+    {
+        for (int i = 0; i < menuTabs.Length; i++)
+        {
+            if (menuTabs[i].activeSelf)
+            {
+                SetButtonColorToActive(menuButtons[i]);
+            }
+            else
+            {
+                SetButtonColorToInactive(menuButtons[i]);
+            }
+        }
+    }
+
+    private void SetButtonColorToInactive(Button button)
+    {
+        ColorBlock colors = button.colors;
+        colors.normalColor = colors.pressedColor;
+        button.colors = colors;
+    }
+
+    private void SetButtonColorToActive(Button button)
+    {
+        ColorBlock colors = button.colors;
+        colors.normalColor = colors.disabledColor;
+        button.colors = colors;
+    }
+
+    public void SwitchPauseMenuTab(int tabNum)
+    {
+        for (int i = 0; i < menuTabs.Length; i++)
+        {
+            if(i != tabNum)
+            {
+                menuTabs[i].SetActive(false);
+            }
+            else
+            {
+                menuTabs[i].SetActive(true);
+            }
+        }
+
+        HighlightButtonForSelectedTab();
+    }
+}
