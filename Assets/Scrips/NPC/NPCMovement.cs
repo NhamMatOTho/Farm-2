0fd32f552cb92bd0c77ef4e2b712db0f5be63001
NhamMatOTho <Hoangtrongluoi@gmail.com>
2024-10-28 17:34:03 +0700

    Save/Load NPC
    Save/Load NPC's position
    Add another NPC-Cora

--- Assets/Scrips/NPC/NPCMovement.cs
+++ Assets/Scrips/NPC/NPCMovement.cs
@@ -11,12 +11,12 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(BoxCollider2D))]
 public class NPCMovement : MonoBehaviour
 {
-    [HideInInspector] public SceneName npcCurrentScene;
+    public SceneName npcCurrentScene;
     [HideInInspector] public SceneName npcTargetScene;
     [HideInInspector] public Vector3Int npcCurrentGridPosition;
     [HideInInspector] public Vector3Int npcTargetGridPosition;
     [HideInInspector] public Vector3 npcTargetWorldPosition;
-    [HideInInspector] public Direction npcFacingDirectionAtDestination;
+    public Direction npcFacingDirectionAtDestination;
 
     private SceneName npcPreviousMovementStepScene;
     private Vector3Int npcNextGridPosition;
@@ -271,6 +271,24 @@ public class NPCMovement : MonoBehaviour
         Vector3 worldPosition = grid.CellToWorld(gridPosition);
 
         return new Vector3(worldPosition.x + Settings.gridCellSize / 2f, worldPosition.y + Settings.gridCellSize / 2f, worldPosition.z);
+    }
+
+    public void CancelNPCMovement()
+    {
+        npcPath.ClearPath();
+        npcNextGridPosition = Vector3Int.zero;
+        npcNextWorldPosition = Vector3.zero;
+        npcIsMoving = false;
+
+        if(moveToGridPositionRoutine != null)
+        {
+            StopCoroutine(moveToGridPositionRoutine);
+        }
+
+        ResetMoveAnimation();
+        ClearNPCEventAnimation();
+        npcTargetAnimationClip = null;
+        SetIdleAnimation();
     }
 
     private void InitialiseNPC()
